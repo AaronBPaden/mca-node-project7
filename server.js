401f191dde325f3e2240e93df7d53c433fe4916a
Aaron Paden <aaronbpaden@gmail.com>
2022-03-24 14:14:49 -0500

    Initial server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+"use strict";
+import express from 'express';
+import config from './config/config.js';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.listen(config.PORT, config.HOST, () => console.log(`Listening on ${config.HOST}:${config.PORT}`));
