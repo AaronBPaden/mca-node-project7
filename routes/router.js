fde747226969703828f89b2ca1695a2ca1671262
Aaron Paden <aaronbpaden@gmail.com>
2022-03-24 14:28:10 -0500

    Add homepage.

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,15 @@
 import express from 'express';
 const router = express.Router();
 
+router.use(express.static('public'));
+
+router.get('/', (req, res) => {
+	res.render('pages/home', {
+		title: 'Countries home',
+		name: 'Countries'
+	});
+});
+
 router.get('*', (req, res) => {
 	switch(req.url) {
 		case '/favicon.ico':
